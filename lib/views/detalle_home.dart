bcbd1714af3f947c77f04994e92cbd5067e3e839
MiurelRivas09 <rivasmiurel01@gmail.com>
2021-10-10 03:32:37 -0600

    Feat: Vista home page

---
+++ lib/views/detalle_home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetalleHome extends StatelessWidget {
+  const DetalleHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
